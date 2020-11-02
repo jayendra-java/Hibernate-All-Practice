65d293eb2302d4787f2f346c2dedc60425f36755
akhil <akhil@LAPTOP-HMM5O8QN>
2020-11-02 13:50:46 +0530

    save vs saveorUpdate

--- src/main/java/com/hibernate/util/Hibpra/SessionAndSave.java
+++ src/main/java/com/hibernate/util/Hibpra/SessionAndSave.java
@@ -18,12 +18,37 @@ public class SessionAndSave {
 		SessionFactory sf=Example.getSessionFactory();
 		Session s=sf.openSession();
 		org.hibernate.Transaction tx=s.beginTransaction();
-		Student s1=new Student("nani",25,"C.S.E");
-		s.save(s1);
+		Student s1=new Student("phani",26,"E.C.E");
+		s.saveOrUpdate(s1);
+		s1.setName("suresh");
+		System.out.println(s1);
+		//System.out.println("saved object with "+i); 
 		tx.commit();
+		Session s2=sf.openSession();
+		org.hibernate.Transaction tx2=s2.beginTransaction();
+		s1.setBranch("def");
+		s2.saveOrUpdate(s1);
+		tx2.commit();	
+		System.out.println(s1);
+		Session se=sf.openSession();
+		org.hibernate.Transaction tx3=se.beginTransaction();
+		Student sr=new Student("phani",26,"E.C.E");
+		se.save(sr);
+		sr.setName("suresh");
+		//System.out.println("saved object with "+i); 
+		tx3.commit();
+		Session s12=sf.openSession();
+		org.hibernate.Transaction tx12=s12.beginTransaction();
+		sr.setBranch("def");
+		s12.save(sr);
+		tx12.commit();
+		
+		
+		System.out.println("hello world hibernate");
+		
     }
 	
 	
 	
-
-}
+	
+	}
